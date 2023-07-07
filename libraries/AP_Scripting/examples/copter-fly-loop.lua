b2b2938620ae3f54fb2853269c98ba4623a6b69f
Kaneyasu Asakuma <g.kasak@gmail.com>
2023-07-07 23:09:43 +0900

    Copter: Add yaw target to loop flying
    Signed-off-by: Kaneyasu Asakuma <g.kasak@gmail.com>

--- libraries/AP_Scripting/examples/copter-fly-loop.lua
+++ libraries/AP_Scripting/examples/copter-fly-loop.lua
@@ -4,26 +4,28 @@
 -- this script waits for the vehicle to be armed and RC6 input > 1800 and then:
 --    a) switches to Guided mode
 --    b) takeoff to 'takeoff_alt_above_home' meters
---    c) cruise to north direction with 'cruise_counter_max_north / 10' sec
---    d) pitch up with 'pitch_up_rate1' deg/sec
---    e) cruise to south direction with 'cruise_counter_max_south / 10' sec
---    f) pitch up with 'pitch_up_rate2' deg/sec
---    g) goto (c)
+--    c) rotate to 'yaw_target'
+--    d) cruise to x direction for 'cruise_counter_max1 / 10' sec
+--    e) pitch up with 'pitch_up_rate1' deg/sec
+--    f) cruise to -x direction for 'cruise_counter_max2 / 10' sec
+--    g) pitch up with 'pitch_up_rate2' deg/sec
+--    h) goto (d)
 
 local takeoff_alt_above_home = 20  -- m
 local copter_guided_mode_num = 4
 local stage = 0
-local cruise_speed = 10.0  -- m/sec
 local cruise_counter = 0
-local cruise_counter_max_north = 30  -- counts (10counts = 1sec)
-local cruise_counter_max_south = 35  -- counts (10counts = 1sec)
-local cruise_pitch_angle_north = -30.0  -- deg
-local cruise_pitch_angle_south =  30.0  -- deg
+local cruise_counter_max1 = 30  -- counts (10counts = 1sec)
+local cruise_counter_max2 = 35  -- counts (10counts = 1sec)
+local cruise_pitch_angle1 = -30.0  -- deg
+local cruise_pitch_angle2 =  30.0  -- deg
 local pitch_up_rate1 = 48.0  -- deg/sec
 local pitch_up_angle_threshold1 = 20  -- deg
 local pitch_up_rate2 = 160.0  -- deg/sec
 local pitch_up_angle_threshold2 = -70  -- deg
 local pitch_deg_prev = 1.0e8
+
+local yaw_target = 0.0
 
 -- the main update function that uses the takeoff and velocity controllers to fly a rough square pattern
 function update()
@@ -34,14 +36,15 @@ function update()
     if pwm6 and pwm6 > 1800 then    -- check if RC6 input has moved high
       if (stage == 0) then          -- change to guided mode
         if (vehicle:set_mode(copter_guided_mode_num)) then  -- change to Guided mode
-          stage = stage + 1
+          stage = 1
+          gcs:send_text(0, "transition to stage1: Takeoff")
         end
 
       elseif (stage == 1) then      -- Stage1: takeoff
         if (vehicle:start_takeoff(takeoff_alt_above_home)) then
-          stage = stage + 1
+          stage = 2
           gcs:send_text(0, "transition to stage2: Alt hold")
-        end
+      end
 
       elseif (stage == 2) then      -- Stage2: altitude hold
         local home = ahrs:get_home()
@@ -49,32 +52,40 @@ function update()
         if home and curr_loc then
           local vec_from_home = home:get_distance_NED(curr_loc)
           if (math.abs(takeoff_alt_above_home + vec_from_home:z()) < 1) then
-            stage = stage + 1
-            gcs:send_text(0, "transition to stage3: Cruise to North")
+            stage = 3
+            gcs:send_text(0, "transition to stage3: Yaw rotation")
           end
         end
 
-      elseif (stage == 3) then   -- Stage3: cruise to north
+      elseif (stage == 3) then  -- Stage3: yaw rotation
         cruise_counter = cruise_counter + 1
 
-        local target_vel = Vector3f()
-        target_vel:x(cruise_speed)
-        target_vel:y(0.0)
-        target_vel:z(0.0)
-
-        if (cruise_counter > cruise_counter_max_north) then
-          stage = stage + 1
-          gcs:send_text(0, "transition to stage4: Pitch up")
-        end
-
-        -- -- send angle request
-        if not (vehicle:set_target_angle_and_climbrate(0.0, cruise_pitch_angle_north, 0.0, 0.0, false, 0.0)) then
+        if not (vehicle:set_target_angle_and_climbrate(0.0, 0.0, yaw_target, 0.0, false, 0.0)) then
           gcs:send_text(0, "failed to execute angle command")
         end
 
-      elseif (stage == 4) then  -- Stage4: pitch up and transition to cruise to south
+        if cruise_counter > 100 then
+          cruise_counter = 0
+          stage = 4
+          gcs:send_text(0, "transition to stage4: Cruise to X")
+        end
+
+      elseif (stage == 4) then   -- Stage4: cruise to X
+        cruise_counter = cruise_counter + 1
+
+        if (cruise_counter > cruise_counter_max1) then
+          stage = 5
+          gcs:send_text(0, "transition to stage5: Pitch up")
+        end
+
+        -- -- send angle request
+        if not (vehicle:set_target_angle_and_climbrate(0.0, cruise_pitch_angle1, yaw_target, 0.0, false, 0.0)) then
+          gcs:send_text(0, "failed to execute angle command")
+        end
+
+      elseif (stage == 5) then  -- Stage5: pitch up and transition to cruise to -X
         local roll_rate_rs = 0.0
-        local pitch_rate_rs = pitch_up_rate1 * 3.14 / 180.0
+        local pitch_rate_rs = pitch_up_rate1 * math.rad(180.0) / 180.0
         local yaw_rate_rs = 0.0
         local thrust = 1.0
 
@@ -83,35 +94,30 @@ function update()
           gcs:send_text(0, "failed to execute rate command")
         end
 
-        local pitch_deg = ahrs:get_pitch() * 180.0 / 3.14
+        local pitch_deg = ahrs:get_pitch() * 180.0 / math.rad(180.0)
         if (pitch_deg > pitch_up_angle_threshold1) then
-          stage = stage + 1
+          stage = 6
           cruise_counter = 0
-          gcs:send_text(0, "transition to stage5: Cruise to South")
+          gcs:send_text(0, "transition to stage6: Cruise to -X")
         end
 
-      elseif (stage == 5) then  -- Stage5: cruise to -x (NED)
+      elseif (stage == 6) then  -- Stage6: cruise to -x
         cruise_counter = cruise_counter + 1
 
-        local target_vel = Vector3f()
-        target_vel:x(-cruise_speed)
-        target_vel:y(0.0)
-        target_vel:z(0.0)
-
-        if (cruise_counter > cruise_counter_max_south) then
-          stage = stage + 1
+        if (cruise_counter > cruise_counter_max2) then
+          stage = 7
           pitch_deg_prev = 1.0e8
-          gcs:send_text(0, "transition to stage6: Pitch up")
+          gcs:send_text(0, "transition to stage7: Pitch up")
         end
 
         -- send angle request
-        if not (vehicle:set_target_angle_and_climbrate(0.0, cruise_pitch_angle_south, 0.0, 0.0, false, 0.0)) then
+        if not (vehicle:set_target_angle_and_climbrate(0.0, cruise_pitch_angle2, yaw_target, 0.0, false, 0.0)) then
           gcs:send_text(0, "failed to execute angle command")
         end
 
-      elseif (stage == 6) then  -- Stage6: pitch up and transition to cruise to north
+      elseif (stage == 7) then  -- Stage6: pitch up and transition to cruise to X
         local roll_rate_rs = 0.0
-        local pitch_rate_rs = pitch_up_rate2 * 3.14 / 180.0
+        local pitch_rate_rs = pitch_up_rate2 * math.rad(180.0) / 180.0
         local yaw_rate_rs = 0.0
         local thrust = 0.0
 
@@ -120,11 +126,11 @@ function update()
           gcs:send_text(0, "failed to execute rate command")
         end
 
-        local pitch_deg = ahrs:get_pitch() * 180.0 / 3.14
+        local pitch_deg = ahrs:get_pitch() * 180.0 / math.rad(180.0)
         if (pitch_deg < 0.0 and pitch_deg > pitch_up_angle_threshold2 and pitch_deg > pitch_deg_prev) then
-          stage = 3  -- north cruise
+          stage = 4  -- X cruise
           cruise_counter = 0
-          gcs:send_text(0, "transition to stage3: Cruise to North")
+          gcs:send_text(0, "transition to stage4: Cruise to X")
         end
         pitch_deg_prev = pitch_deg
       end
@@ -136,8 +142,11 @@ function update()
         if home and curr_loc then
           local vec_from_home = home:get_distance_NED(curr_loc)
           local alt = -vec_from_home:z()
-          local x = vec_from_home:x()
-          local y = vec_from_home:y()
+          local north = vec_from_home:x()
+          local east = vec_from_home:y()
+          local yaw_target_rad = yaw_target * math.rad(180.0) / 180.0
+          local x = north * math.cos(yaw_target_rad) + east * math.sin(yaw_target_rad)
+          local y = -north * math.sin(yaw_target_rad) + east * math.cos(yaw_target_rad)
           if alt < 10 or 50 < alt or x < -10 or 40 < x or math.abs(y) > 10 then
             vehicle:set_mode(6)
             gcs:send_text(0, "Geo fence violation: transition to RTL")
